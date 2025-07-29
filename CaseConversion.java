426367438b44c6bd3a977c04a90113b826991129
Manognya399 <cheekotimanognya@gmail.com>
2025-07-29 11:30:17 +0530

    added caseconversion file

---
+++ CaseConversion.java
@@ -0,0 +1,10 @@
+package OOPS_Practical;
+
+public class CaseConversion {
+	public static void main(String[] args) {
+		String s="Hello world";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+	}
+
+}
